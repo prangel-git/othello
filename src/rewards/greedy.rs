45868e8f3f0ba736a530fde572d5fba8e406141d
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-05-16 16:20:02 -0400

    Changing greedy reward to take into account which agent is asking.

--- src/rewards/greedy.rs
+++ src/rewards/greedy.rs
@@ -1,5 +1,11 @@
+use gts::abstractions::Environment;
+
 use crate::{AgentId, Board};
 
-pub fn greedy_reward(env: &Board, _agent: &AgentId) -> f64 {
-    env.score() as f64
+pub fn greedy_reward(env: &Board, &agent: &AgentId) -> f64 {
+    if agent == env.turn() {
+        env.score() as f64
+    } else {
+        -env.score() as f64
+    }
 }
